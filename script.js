d410dfd0be5773183224418a53594588fb3bd377
Huber <164633287+HuberServices@users.noreply.github.com>
2024-07-08 21:51:50 -0500

    preloader

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+window.addEventListener('load', function() {
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+});
